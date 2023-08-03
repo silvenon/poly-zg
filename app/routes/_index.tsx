1252ca63be65c1a96c270de40ed23617a4682161
Matija Marohnić <matija.marohnic@gmail.com>
2023-08-04 01:29:08 +0200

    Add home page description

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,14 @@
 import type { V2_MetaFunction } from "@remix-run/node";
 
-export const meta: V2_MetaFunction = () => [{ title: "Poly Zagreb" }];
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: "Poly Zagreb" },
+    {
+      name: "description",
+      content: "Poliamorna zajednica u Zagrebu",
+    },
+  ];
+};
 
 export default function Index() {
   return (
