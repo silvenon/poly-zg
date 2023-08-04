60a10c1e516dff6810bba3c944f781337338413c
Matija Marohnić <matija.marohnic@gmail.com>
2023-08-04 19:54:21 +0200

    Remove indicator of external link in books

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -1,5 +1,4 @@
 import type { PropsWithChildren } from "react";
-import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid";
 
 interface Props extends PropsWithChildren<{}> {
   title: string;
@@ -30,17 +29,11 @@ export default function Function({
         href={href}
         target="_blank"
         rel="noreferrer"
-        className="group aspect-h-[500] aspect-w-[332] relative block overflow-hidden rounded-xl ring ring-transparent transition hover:ring-red-500 hover:ring-offset-2 dark:ring-offset-stone-900 dark:hover:ring-red-400"
+        className="aspect-h-[500] aspect-w-[332] relative block overflow-hidden rounded-xl ring ring-transparent transition hover:ring-red-500 hover:ring-offset-2 dark:ring-offset-stone-900 dark:hover:ring-red-400"
         aria-label={linkLabel}
       >
         <div>
           <img alt="" src={cover} className="absolute inset-0 object-cover" />
-          <div className="absolute right-0 top-0 rounded-bl-lg border-b border-l border-black/10 bg-black/30 p-1 transition-colors group-hover:bg-red-500">
-            <ArrowTopRightOnSquareIcon
-              aria-hidden="true"
-              className="h-6 w-6 text-white"
-            />
-          </div>
         </div>
       </a>
     </article>
