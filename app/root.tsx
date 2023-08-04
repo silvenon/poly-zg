7da484883e5aec28b56fe92cad3920db2172a199
Matija Marohnić <matija.marohnic@gmail.com>
2023-08-05 00:10:01 +0200

    Add icons

--- app/root.tsx
+++ app/root.tsx
@@ -13,7 +13,16 @@ import stylesheet from "~/tailwind.css";
 import Header from "~/components/Header";
 import NavBar from "./components/NavBar";
 
+// https://evilmartians.com/chronicles/how-to-favicon-in-2021-six-files-that-fit-most-needs
+const iconLinks = [
+  { rel: "icon", href: "/favicon.ico", sizes: "32x32" },
+  { rel: "icon", href: "/icon.svg", type: "image/svg+xml" },
+  { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
+  { rel: "manifest", href: "/manifest.webmanifest" },
+];
+
 export const links: LinksFunction = () => [
+  ...iconLinks,
   { rel: "stylesheet", href: stylesheet },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
