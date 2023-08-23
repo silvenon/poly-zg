239f0987926c8d3f513aa471eb479ec9b54cf908
Matija Marohnić <matija.marohnic@gmail.com>
2023-08-24 00:17:25 +0200

    Incorporate diagram of types of nonmonogamy

--- app/components/Prose.tsx
+++ app/components/Prose.tsx
@@ -1,6 +1,13 @@
-export default function Prose({ children }: { children?: React.ReactNode }) {
+import clsx from "clsx";
+
+interface Props {
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export default function Prose({ className, children }: Props) {
   return (
-    <div className="px-4 py-6 sm:py-8 md:py-10">
+    <div className={clsx("px-4 py-6 sm:py-8 md:py-10", className)}>
       <div className="prose prose-sm prose-stone mx-auto dark:prose-invert sm:prose-base md:prose-lg lg:prose-xl">
         {children}
       </div>
