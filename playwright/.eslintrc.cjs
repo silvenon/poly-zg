302a8df0c643ea9f7050bd5bf7dd8d16196827e9
Matija Marohnić <matija.marohnic@gmail.com>
2024-02-10 15:40:56 +0100

    Connect TypeScript configs with references

--- playwright/.eslintrc.cjs
+++ playwright/.eslintrc.cjs
@@ -1,9 +1,5 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: "./tsconfig.json",
-  },
   rules: {
     "testing-library/prefer-screen-queries": "off",
   },
