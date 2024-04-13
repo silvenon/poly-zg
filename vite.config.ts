1ba0ce1925d78d42744f358eb099053a7150406b
Matija Marohnić <matija.marohnic@gmail.com>
2024-04-13 16:20:53 +0200

    Bump Remix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 import mdx from "@mdx-js/rollup";
 import { defineConfig } from "vite";
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
+import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { metronome } from "metronome-sh/vite";
 
